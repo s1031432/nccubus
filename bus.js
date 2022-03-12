d3ecdccb01260cadc5edb5c97c278ceb137431ed
Huang, Yi-Tang <ty80517@gmail.com>
2022-03-13 05:55:38 +0800

    Promise

--- bus.js
+++ bus.js
@@ -90,52 +90,50 @@ function getData(mode){
     }
     // Call ptx API to get bus data(json)
     // More infomation: https://ptx.transportdata.tw/MOTC/?urls.primaryName=%E5%85%AC%E8%BB%8AV2#/Bus%20Advanced(By%20Station)/CityBusApi_EstimatedTimeOfArrival_ByStation_2880
-    return new Promise(resolve => { 
-        request(`https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/PassThrough/Station/${stationID}?%24top=30&%24format=JSON`,{
-            headers: GetAuthorizationHeader(),
-            gzip: true
-        }, function(error, response, body){
-            if(error)
-                resolve(error)
-            body = JSON.parse(body);
-            body = sortBusData(body);
-            let result = [str,"--"];
-            for(var i=0;i<body.length;i++){
-                if( (whiteList0.indexOf(body[i].RouteName.En)>-1 && body[i].Direction==0)  || (whiteList1.indexOf(body[i].RouteName.En)>-1 && body[i].Direction==1)){
-                    str = `${body[i].RouteName.Zh_tw}`;
-                    if(body[i].StopStatus == 0){
-                        str = body[i].EstimateTime < 180 ? `✅ ${str} - 即將進站` : `✅ ${str} - 約${parseInt(body[i].EstimateTime/60)}分`;
-                    }
-                    else if(body[i].StopStatus == 1){
-                        if(body[i].EstimateTime){
-                            str = body[i].EstimateTime < 180 ? `✅ ${str} - 即將進站` : `✅ ${str} - 約${parseInt(body[i].EstimateTime/60)}分（尚未發車）`;
-                        }
-                        else if(body[i].EstimateTime == undefined){
-                            str = `💤 ${str} - 尚未發車`;
-                        }
-                    }
-                    else if(body[i].StopStatus == 2){
-                        str = `⚠️ ${str} - 交管不停靠`;
-                    }
-                    else if(body[i].StopStatus == 3){
-                        str = `❌ ${str} - 末班車已過`;
-                    }
-                    else if(body[i].StopStatus == 4){
-                        str = `❌ ${str} - 今日未營運`;
-                    }
-                    if(body[i].IsLastBus){
-                        str += ` 🔴末班車！`;
-                    }
-                    result.push(str);
+
+    request(`https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/PassThrough/Station/${stationID}?%24top=30&%24format=JSON`,{
+        headers: GetAuthorizationHeader(),
+        gzip: true
+    }, function(error, response, body){
+        if(error)
+            console.log('err', error)
+        body = JSON.parse(body);
+        body = sortBusData(body);
+        let result = [str,"--"];
+        for(var i=0;i<body.length;i++){
+            if( (whiteList0.indexOf(body[i].RouteName.En)>-1 && body[i].Direction==0)  || (whiteList1.indexOf(body[i].RouteName.En)>-1 && body[i].Direction==1)){
+                str = `${body[i].RouteName.Zh_tw}`;
+                if(body[i].StopStatus == 0){
+                    str = body[i].EstimateTime < 180 ? `✅ ${str} - 即將進站` : `✅ ${str} - 約${parseInt(body[i].EstimateTime/60)}分`;
                 }
+                else if(body[i].StopStatus == 1){
+                    if(body[i].EstimateTime){
+                        str = body[i].EstimateTime < 180 ? `✅ ${str} - 即將進站` : `✅ ${str} - 約${parseInt(body[i].EstimateTime/60)}分（尚未發車）`;
+                    }
+                    else if(body[i].EstimateTime == undefined){
+                        str = `💤 ${str} - 尚未發車`;
+                    }
+                }
+                else if(body[i].StopStatus == 2){
+                    str = `⚠️ ${str} - 交管不停靠`;
+                }
+                else if(body[i].StopStatus == 3){
+                    str = `❌ ${str} - 末班車已過`;
+                }
+                else if(body[i].StopStatus == 4){
+                    str = `❌ ${str} - 今日未營運`;
+                }
+                if(body[i].IsLastBus){
+                    str += ` 🔴末班車！`;
+                }
+                result.push(str);
             }
-            let nowMs = (+new Date())+8*60*60*1000;
-            result.push(`--`);
-            result.push(`資料最後更新時間\n${getDateTime.getDateTime(new Date(nowMs))}</pre>`);
-            console.log(`${mode} data update`)
-            updateBusResult(mode, result);
-            resolve(200);
-        });
+        }
+        let nowMs = (+new Date())+8*60*60*1000;
+        result.push(`--`);
+        result.push(`資料最後更新時間\n${getDateTime.getDateTime(new Date(nowMs))}</pre>`);
+        console.log(`${mode} data update`)
+        updateBusResult(mode, result);
     });
 }
 function sortBusData(body){
@@ -220,12 +218,12 @@ bot.onText(/\/nccu1$/, (msg) => {
 
 
 async function main(){
-    // prevent API response error by setTimeout()
-    await getData("zoo_nccu1");
-    await getData("nccu_zoo");
-    await getData("nccu1_zoo");
-    await getData("xinguang");
-    await getData("nccu1");
+
+    getData("zoo_nccu1");
+    getData("nccu_zoo");
+    getData("nccu1_zoo");
+    getData("xinguang");
+    getData("nccu1");
     setTimeout(main, 20000)
 }
 
