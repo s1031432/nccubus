41975b0183e6a156cf594901be4223c222b9f895
yt-MIG-win10 <ty80517@gmail.com>
2022-03-13 14:35:15 +0800

    data[]

--- bus.js
+++ bus.js
@@ -16,31 +16,46 @@ const bot = new telegramBot(token, {polling: true});
 
 
 data = {"zoo_nccu1":{}, "nccu_zoo":{}, "nccu1_zoo":{}, "xinguang":{}, "nccu1":{}};
+
 // 捷運動物園站 往 政大一站
 data.zoo_nccu1.stationID = 2442;
+data.zoo_nccu1.whiteList = [];
 data.zoo_nccu1.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "BR6", "282", "66", "676", "611"];
 data.zoo_nccu1.whiteList[1] = ["G1", "BR18", "933"];
 data.zoo_nccu1.str = "<pre>➡️ 動物園站(往政大)";
+data.zoo_nccu1.lastUpdateTime = getNowTime();
+
 // 政大站(麥側萊爾富) 往 動物園站
 data.nccu_zoo.stationID = 2415;
+data.nccu_zoo.whiteList = [];
 data.nccu_zoo.whiteList[0] = ["933", "BR18", "G1"];
 data.nccu_zoo.whiteList[1] = ["236Shuttle", "282", "295", "295Sub", "611", "66", "679", "BR6", "Roosevelt Rd. Metro Bus"];
 data.nccu_zoo.str = "<pre>➡️ 政大站(麥側萊爾富往動物園)";
+data.nccu_zoo.lastUpdateTime = getNowTime();
+
 // 政大一站(Jason前) 往 動物園站
 data.nccu1_zoo.stationID = 1001400;
+data.nccu1_zoo.whiteList = [];
 data.nccu1_zoo.whiteList[0] = ["933", "G1"];
 data.nccu1_zoo.whiteList[1] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "237", "66"];
 data.nccu1_zoo.str = "<pre>➡️ 政大一站(Jason對面往動物園)";
+data.nccu1_zoo.lastUpdateTime = getNowTime();
+
 // 新光路口站的所有公車
 data.xinguang.stationID = 1000854;
+data.xinguang.whiteList = [];
 data.xinguang.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "282", "295", "295Sub", "530", "611", "66", "676", "679", "BR11", "BR11Sub", "BR3", "BR6"];
 data.xinguang.whiteList[1] = ["933", "S10", "S10Shuttle", "BR5", "G1"];
 data.xinguang.str = "<pre>➡️ 新光路口(龍角前)";
+data.xinguang.lastUpdateTime = getNowTime();
+
 // 政大一(校門前)的所有公車
 data.nccu1.stationID = 1001409;
+data.nccu1.whiteList = [];
 data.nccu1.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "237", "282", "530", "611", "66", "676", "BR6"];
 data.nccu1.whiteList[1] = ["933", "G1"];
 data.nccu1.str = "<pre>➡️ 政大一(校門前)";
+data.nccu1.lastUpdateTime = getNowTime();
 
 function GetAuthorizationHeader() {
     // Get AppID & AppKey: https://ptx.transportdata.tw/PTX/
@@ -55,15 +70,10 @@ function GetAuthorizationHeader() {
     return { 'Authorization': Authorization, 'X-Date': GMTString ,'Accept-Encoding': 'gzip'}; 
 }
 
-var zoo_nccu1_data = "";
-var nccu_zoo_data = "";
-var nccu1_zoo_data = "";
-var xinguang_data = "";
-var nccu1_data = "";
 
 function getData(mode){
     console.log(`getData(${mode});`)
-    let now = getDateTime.getDateTime(new Date((+new Date())+8*60*60*1000));
+    let now = getDateTime.getDateTime(getNowTime());
     let hours = now[11]+now[12];
 
     // 02:00 ~ 05:00 don't call api
@@ -74,7 +84,7 @@ function getData(mode){
     // Call ptx API to get bus data(json)
     // More infomation: https://ptx.transportdata.tw/MOTC/?urls.primaryName=%E5%85%AC%E8%BB%8AV2#/Bus%20Advanced(By%20Station)/CityBusApi_EstimatedTimeOfArrival_ByStation_2880
 
-    request(`https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/PassThrough/Station/${stationID}?%24top=30&%24format=JSON`,{
+    request(`https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/PassThrough/Station/${data[mode].stationID}?%24top=30&%24format=JSON`,{
         headers: GetAuthorizationHeader(),
         gzip: true,
         timeout: 2500,
@@ -165,7 +175,9 @@ function sortBusData(body){
     }
     return body;
 }
-
+function getNowTime(){
+    return new Date((+new Date())+8*60*60*1000);
+}
 bot.onText(/\/start$/, (msg) => {
     console.log(msg);
     var replyMsg = "";
