815897de475b343322f14d184e1a239043855076
yt-MIG-win10 <ty80517@gmail.com>
2022-03-13 15:05:52 +0800

    data[]

--- bus.js
+++ bus.js
@@ -22,40 +22,45 @@ data.zoo_nccu1.stationID = 2442;
 data.zoo_nccu1.whiteList = [];
 data.zoo_nccu1.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "BR6", "282", "66", "676", "611"];
 data.zoo_nccu1.whiteList[1] = ["G1", "BR18", "933"];
-data.zoo_nccu1.str = "<pre>➡️ 動物園站(往政大)";
-data.zoo_nccu1.lastUpdateTimeMs = (+new Date())+8*60*60*1000
+data.zoo_nccu1.title = "<pre>➡️ 動物園站(往政大)";
+data.zoo_nccu1.str = "";
+data.zoo_nccu1.lastUpdateTimeMs = (+new Date())+8*60*60*1000;
 
 // 政大站(麥側萊爾富) 往 動物園站
 data.nccu_zoo.stationID = 2415;
 data.nccu_zoo.whiteList = [];
 data.nccu_zoo.whiteList[0] = ["933", "BR18", "G1"];
 data.nccu_zoo.whiteList[1] = ["236Shuttle", "282", "295", "295Sub", "611", "66", "679", "BR6", "Roosevelt Rd. Metro Bus"];
-data.nccu_zoo.str = "<pre>➡️ 政大站(麥側萊爾富往動物園)";
-data.nccu_zoo.lastUpdateTimeMs = (+new Date())+8*60*60*1000
+data.nccu_zoo.title = "<pre>➡️ 政大站(麥側萊爾富往動物園)";
+data.nccu_zoo.str = "";
+data.nccu_zoo.lastUpdateTimeMs = (+new Date())+8*60*60*1000;
 
 // 政大一站(Jason前) 往 動物園站
 data.nccu1_zoo.stationID = 1001400;
 data.nccu1_zoo.whiteList = [];
 data.nccu1_zoo.whiteList[0] = ["933", "G1"];
 data.nccu1_zoo.whiteList[1] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "237", "66"];
-data.nccu1_zoo.str = "<pre>➡️ 政大一站(Jason對面往動物園)";
-data.nccu1_zoo.lastUpdateTimeMs = (+new Date())+8*60*60*1000
+data.nccu1_zoo.title = "<pre>➡️ 政大一站(Jason對面往動物園)";
+data.nccu1_zoo.str = "";
+data.nccu1_zoo.lastUpdateTimeMs = (+new Date())+8*60*60*1000;
 
 // 新光路口站的所有公車
 data.xinguang.stationID = 1000854;
 data.xinguang.whiteList = [];
 data.xinguang.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "282", "295", "295Sub", "530", "611", "66", "676", "679", "BR11", "BR11Sub", "BR3", "BR6"];
 data.xinguang.whiteList[1] = ["933", "S10", "S10Shuttle", "BR5", "G1"];
-data.xinguang.str = "<pre>➡️ 新光路口(龍角前)";
-data.xinguang.lastUpdateTimeMs = (+new Date())+8*60*60*1000
+data.xinguang.title = "<pre>➡️ 新光路口(龍角前)";
+data.xinguang.str = "";
+data.xinguang.lastUpdateTimeMs = (+new Date())+8*60*60*1000;
 
 // 政大一(校門前)的所有公車
 data.nccu1.stationID = 1001409;
 data.nccu1.whiteList = [];
 data.nccu1.whiteList[0] = ["Roosevelt Rd. Metro Bus", "236Shuttle", "237", "282", "530", "611", "66", "676", "BR6"];
 data.nccu1.whiteList[1] = ["933", "G1"];
-data.nccu1.str = "<pre>➡️ 政大一(校門前)";
-data.nccu1.lastUpdateTimeMs = (+new Date())+8*60*60*1000
+data.nccu1.title = "<pre>➡️ 政大一(校門前)";
+data.nccu1.str = "";
+data.nccu1.lastUpdateTimeMs = (+new Date())+8*60*60*1000;
 
 function GetAuthorizationHeader() {
     // Get AppID & AppKey: https://ptx.transportdata.tw/PTX/
@@ -79,7 +84,7 @@ function getData(mode){
         return data[mode].str;
     }
     if( isDataUpdated(mode) ){
-        console.log("Updated");
+        console.log(`${mode} data is fresh.`);
         return data[mode].str;
     }
     console.log("AAA")
@@ -99,7 +104,7 @@ function getData(mode){
             else{
                 body = JSON.parse(body);
                 body = sortBusData(body);
-                let result = [data[mode].str,"--"];
+                let result = [data[mode].title,"--"];
                 for(var i=0;i<body.length;i++){
                     if( (data[mode].whiteList[0].indexOf(body[i].RouteName.En)>-1 && body[i].Direction==0)  || (data[mode].whiteList[1].indexOf(body[i].RouteName.En)>-1 && body[i].Direction==1)){
                         str = `${body[i].RouteName.Zh_tw}`;
@@ -232,5 +237,5 @@ app.get('/', function (req, res) {
     res.json({ version: packageInfo.version });
 });
 app.listen(process.env.PORT || 5000, function () {
-    console.log('Server is running...');
+    console.log(`${getDateTime.getDateTime(new Date((+new Date())+8*60*60*1000))}Server is running...`);
 });
