6abc67ac76477080b64e241acbfca1a4d34ca550
Huang, Yi-Tang <ty80517@gmail.com>
2022-03-16 00:49:50 +0800

    fetch module bug fixed

--- bus.js
+++ bus.js
@@ -31,7 +31,7 @@ function requestBusData(url) {
         headers: GetAuthorizationHeader(),
         gzip: true,
         json: true,
-        timeout: 300,
+        timeout: 777,
     }).then(response => response.json());
 }
 function getData(mode){
