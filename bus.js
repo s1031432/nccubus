94d90ce25629bfa7132debff911676d6415bf4cd
Huang, Yi-Tang <ty80517@gmail.com>
2022-03-16 01:11:38 +0800

    timeout 777

--- bus.js
+++ bus.js
@@ -31,7 +31,7 @@ function requestBusData(url) {
         headers: GetAuthorizationHeader(),
         gzip: true,
         json: true,
-        timeout: 30000,
+        timeout: 777,
     }).then(response => response.json());
 }
 function getData(mode){
